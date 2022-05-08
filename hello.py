c9560c3fb97c977f6f325bd88c6e2e168d30b24b
Kitiara2 <105160309+Kitiara2@users.noreply.github.com>
2022-05-08 23:11:09 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -33,8 +33,6 @@ with st.echo(code_location='below'):
     df_ro = get_data("https://github.com/Kitiara2/repo/raw/main/robusta_data_cleaned.csv")
     df_ro.rename(columns={'Country_of_Origin': 'Country.of.Origin', 'Clean_Cup': 'Clean.Cup'}, inplace=True)
     df_ar = get_data("https://github.com/Kitiara2/repo/raw/main/arabica_data_cleaned.csv")
-    
-#    df_country = get_data("https://github.com/Kitiara2/repo/raw/main/country.csv")
 
     df = pd.concat([df_ar, df_ro], ignore_index=False)
 #    df = df_ro
@@ -44,7 +42,9 @@ with st.echo(code_location='below'):
         "https://github.com/Kitiara2/repo/raw/main"
     )
     state_geo = f"{url}/countries-land.geo.json"
-    state_unemployment = f"{url}/country.csv"
+    df_countries = f"{url}/country.csv"
+    df_lands = pd.merge(left_on = 'value', right_on = 'Country.of.Origin')
+    df_lands
     state_data = pd.read_csv(state_unemployment)
 
     m = folium.Map(location=[48, -102], zoom_start=3)
