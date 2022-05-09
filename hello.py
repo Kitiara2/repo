fe4fecb8dd9c8360f5cbc5acb9abe2d8e127cf35
Kitiara2 <105160309+Kitiara2@users.noreply.github.com>
2022-05-09 10:20:51 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -55,6 +55,7 @@ with st.echo(code_location='below'):
 
     df_lands_selection = []
     for row in df_lands.itertuples():
+        row['Species']
         if species in row['Species']:
             df_lands_selection.addend(row)
     
