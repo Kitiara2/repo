535943ad2447521db46cabbeaac7e446674185d6
Kitiara2 <105160309+Kitiara2@users.noreply.github.com>
2022-05-13 19:42:23 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -99,7 +99,7 @@ with st.echo(code_location='below'):
         step = dict(
             method = 'restyle',  
             args = [{'visible': [False] * len(fig.data)},
-                   {"title": "Slider switched to step: " + str(df_years["harvest_year"][i][1])}],
+                   {"title": "Slider switched to step: " + str(i)}],
         )
         # Enable trace we want to see
         step['args'][0]['visible'][i] = True
