4238c2ce6bf0841b09efa0b1897f08a41cc7a57c
Kitiara2 <105160309+Kitiara2@users.noreply.github.com>
2022-05-14 22:40:31 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -179,7 +179,7 @@ with st.echo(code_location='below'):
     )
 
     species = st.selectbox(
-        "Species_1", df["Species"].value_counts().iloc[:10].index
+        "Species_1", df["Species"].value_counts().iloc[:30].index
     )
 
 
