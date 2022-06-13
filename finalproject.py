6a7dfe16fffe4b171f361345d33ac505a0ebcab0
Kitiara2 <105160309+Kitiara2@users.noreply.github.com>
2022-06-13 20:52:32 +0300

    Update finalproject.py

--- finalproject.py
+++ finalproject.py
@@ -90,6 +90,6 @@ with st.echo(code_location='below'):
   Давайте считать, что это шанс продемонстрировать умение работать с `pandas`
   """
   
-  df_startups['industry_type'] = df_startups['is_software']*10
+  df_startups['industry_type'] = df_startups['is_software']*'software'
   
   df_startups
