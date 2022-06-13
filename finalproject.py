0c75b0e86dceff87616a6ea41504dcb01c3aaa76
Kitiara2 <105160309+Kitiara2@users.noreply.github.com>
2022-06-13 15:05:58 +0300

    Update finalproject.py

--- finalproject.py
+++ finalproject.py
@@ -4,5 +4,5 @@ with st.echo(code_location='below'):
   st.write("hello")
   
 
-df = get_data("https://github.com/Kitiara2/repo/raw/main/investments_VC.csv")
+df = get_data("https://github.com/Kitiara2/repo/blob/main/investments_VC.csv")
 df
