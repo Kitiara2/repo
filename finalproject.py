bf7d59aa42807dcde8ab428ad53de062368634e4
Kitiara2 <105160309+Kitiara2@users.noreply.github.com>
2022-06-13 15:06:56 +0300

    Update finalproject.py

--- finalproject.py
+++ finalproject.py
@@ -1,4 +1,18 @@
 import streamlit as st
+from streamlit_folium import st_folium
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import altair as alt
+import geopandas as gpd
+from plotly.graph_objs import Scatter, Figure, Layout
+import plotly
+import plotly.graph_objs as go
+import plotly.express as px
+from plotly.subplots import make_subplots
+import json
+import folium
 
 with st.echo(code_location='below'):
   st.write("hello")
